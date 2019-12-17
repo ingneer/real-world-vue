e5720ab27da32cba3331a79b7927a04c33c5475e
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2019-12-17 22:43:57 +0100

    Use named routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Events</router-link> |
-      <router-link to="/event/create">Create Event</router-link>
+      <router-link :to="{ name: 'events' }">Events</router-link> |
+      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create Event</router-link>
     </div>
     <router-view />
   </div>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>List Eventst</h1>
+  <h1>List Events</h1>
 </template>
