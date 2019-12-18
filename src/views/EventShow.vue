c2ff07be4ddaedce85c802c71ee0293717bfcf5d
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2019-12-18 22:50:11 +0100

    Fetch an event data from the API
    Add reactive event reference
    
    Fix the response target

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -3,7 +3,22 @@
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log('There was an error: ' + error.response)
+      })
+  }
 }
 </script>
